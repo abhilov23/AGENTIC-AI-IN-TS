cbd6e6d841f47540e25b8331a30e329ba9f3c8f3
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2026-05-11 23:17:11 +0530

    basic rag completed

---
+++ 1_RAG/src/chat.ts
@@ -0,0 +1,43 @@
+import { embeddings } from './embeddings.js';
+import "dotenv/config";
+import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { model } from './model.js';
+
+
+async function main(){
+    const vectorStore = new Chroma(embeddings, {
+        collectionName: "notes",
+    });
+
+    const retriever = vectorStore.asRetriever({
+        k:2
+    })
+
+    const question = "What is LangChain?";
+
+    const relevantDocs = await retriever.invoke(question);
+
+    console.log(relevantDocs)
+
+      const context = relevantDocs.map((doc) => doc.pageContent).join("\n\n");
+
+      const prompt = `
+      You are a helpful AI assistant.
+
+Answer the user's question using ONLY the provided context.
+
+     Context:
+     ${context}
+
+     Question:
+     ${question}
+      `
+    const response = await model.invoke(prompt);
+
+    console.log(response.content)
+    console.log("\nAI response:\n")
+    console.log(response.content)
+
+}
+
+main()
